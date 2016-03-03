f11d64fe72587538b1a731096f3ab088ffd613b8
宰威 <zaiwei@zaiweideMacBook-Pro.local>
2016-03-03 11:25:27 +0800

    创建文件 并添加label

---
+++ zaiweiGitTest/zaiweiGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zaiweiGitTest
+//
+//  Created by zaiwei on 16/3/3.
+//  Copyright © 2016年 zaiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
